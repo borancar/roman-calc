a4662780ed948ca55c0296579182b556bfc01a81
Boran Car <boran.car@gmail.com>
2018-09-12 19:41:56 +0100

    Start with converting from roman numerals
    The problem is easier and the solution can be used for testing the other
    transformation.

--- roman/roman.go
+++ roman/roman.go
@@ -1,5 +1,7 @@
 package roman
 
+import "strings"
+
 var values = map[string]int{
 	"I": 1,
 	"V": 5,
@@ -12,4 +14,70 @@ var values = map[string]int{
 
 func ToInteger(n string) int {
 	return values[string(n[0])]
+}
+
+func FromInteger(n int) string {
+	var b strings.Builder
+
+	for n >= 1000 {
+		b.WriteString("M")
+		n -= 1000
+	}
+
+	if n >= 900 {
+		b.WriteString("CM")
+		n -= 900
+	}
+
+	if n >= 500 {
+		b.WriteString("D")
+		n -= 500
+	}
+
+	if n >= 400 {
+		b.WriteString("CD")
+		n -= 400
+	}
+
+	for n >= 100 {
+		b.WriteString("C")
+		n -= 100
+	}
+
+	if n >= 90 {
+		b.WriteString("XC")
+		n -= 90
+	}
+
+	if n >= 50 {
+		b.WriteString("L")
+		n -= 50
+	}
+
+	if n >= 40 {
+		b.WriteString("XL")
+		n -= 40
+	}
+
+	for n >= 10 {
+		b.WriteString("X")
+		n -= 10
+	}
+
+	if n >= 9 {
+		b.WriteString("IX")
+		n -= 9
+	}
+
+	if n >= 5 {
+		b.WriteString("V")
+		n -= 5
+	}
+
+	for n >= 1 {
+		b.WriteString("I")
+		n -= 1
+	}
+
+	return b.String()
 }
--- test/roman_test.go
+++ test/roman_test.go
@@ -5,6 +5,12 @@ import "testing"
 
 func TestRomanSingle(t *testing.T) {
 	if v := roman.ToInteger("I"); v != 1 {
-		t.Fatalf("Wrong value for I: %d", v)
+		t.Fatalf("Wrong value for I, expected %d, was %d", 1, v)
+	}
+}
+
+func TestYears(t *testing.T) {
+	if v := roman.FromInteger(2018); v != "MMXVIII" {
+		t.Fatalf("Wrong value for 2018, expected %s, was %s", "MMXVIII", v)
 	}
 }
